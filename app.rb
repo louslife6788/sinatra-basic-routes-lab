3259c4604dbd85f8476224b6a665f53cd52d0584
luis leon <leonluis380@yahoo.com>
2022-01-02 22:29:35 -0500

    commit tree 72fab4c47e809cd8a598dbbcbe67e378898fb5dd

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+    get '/name' do
+        "My name is lou"
+
+    end
+
+    get '/hometown' do 
+        "My hometown is Washingtonville"
+    
+    end
+
+    get '/favorite-song' do
+            "My favorite song is motor bike"
+
+    end
+
+    
+
+    
 end
